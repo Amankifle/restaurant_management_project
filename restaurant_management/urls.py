a98a36bfafd2d2fcb9c4702ee19fd507f0a3a66d
Amankifle <96658675+Amankifle@users.noreply.github.com>
2025-08-13 18:57:13 +0300

    31253

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from home.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('home.urls')),
+    path('',homepage, name='homepage')
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
